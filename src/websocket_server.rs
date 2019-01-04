307d247d602fff945ddc8af02a547a4adb211b21
Finn Sell <dev@finn-thorben.me>
2019-01-04 23:34:37 +0100

    Better error message on websocket send failure

--- src/websocket_server.rs
+++ src/websocket_server.rs
@@ -53,7 +53,8 @@ fn handle_request(map: Arc<Mutex<Vec<Vec<String>>>>, request: WsUpgrade<TcpStrea
                 }
             }
 
-            client.send_message(&OwnedMessage::Text(msg)).unwrap();
+            client.send_message(&OwnedMessage::Text(msg))
+                .expect(format!("Error sending new state to {}", ip).as_str());
 
             // Wait 100ms until another update is sent
             thread::sleep(time::Duration::from_millis(100));
