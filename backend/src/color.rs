f55447a15503992255e5fd361e99148a2bf050bf
Finn-Thorben Sell <dev@finn-thorben.me>
2020-09-14 19:57:25 +0200

    remove old non-working rust backend

--- backend/src/color.rs
+++
@@ -1,5 +0,0 @@
-pub type Color = u32;
-
-pub fn color_from_rgba(r: u8, g: u8, b: u8) -> Color {
-    return (r as u32) << 16 | (g as u32) << 8 | (b as u32);
-}
--- backend/src/network/mod.rs
+++
@@ -1,7 +0,0 @@
-extern crate futures;
-
-mod patient_writer;
-
-pub mod protocol;
-pub mod px_server;
-pub mod tcp_server;
