5547e5b06cc004ba6b4b8cfa8f4b7df6552eb88b
Finn-Thorben Sell <dev@finn-thorben.me>
2020-11-03 15:50:09 +0100

    list frame handling logic out of tcp server

--- rust/src/net/mod.rs
+++ rust/src/net/mod.rs
@@ -1,2 +1,59 @@
+use crate::i18n::get_catalog;
+use crate::net::framing::Frame;
+use crate::parser;
+use crate::parser::command::*;
+use crate::pixmap::SharedPixmap;
+
 mod framing;
 pub mod tcp_server;
+
+fn handle_frame(input: Frame, pixmap: &SharedPixmap) -> Option<Frame> {
+    // try parse the received frame as command
+    let command = match input {
+        Frame::Simple(command_str) => match parser::simple::parse(&command_str) {
+            Ok((_, command)) => Ok(command),
+            Err(_e) => Err("unhelpful, unexplained, generic error"), // TODO improve parser error handling
+        },
+    };
+
+    // handle the command and construct an appropriate response
+    match command {
+        Err(e) => Some(Frame::Simple(format!(
+            "There was an error parsing your command: {}",
+            e
+        ))),
+        Ok(cmd) => match handle_command(cmd, pixmap) {
+            Err(e) => Some(Frame::Simple(format!(
+                "There was an error handling your command: {}",
+                e
+            ))),
+            Ok(None) => None,
+            Ok(Some(response)) => Some(Frame::Simple(response)),
+        },
+    }
+}
+
+fn handle_command(
+    cmd: Command,
+    pixmap: &SharedPixmap,
+) -> core::result::Result<Option<String>, String> {
+    match cmd {
+        Command::Size => Ok(Some(format!(
+            "SIZE {} {}",
+            pixmap.get_size().0,
+            pixmap.get_size().1
+        ))),
+        Command::Help(HelpTopic::General) => Ok(Some(i18n!(get_catalog(), "help_general"))),
+        Command::Help(HelpTopic::Size) => Ok(Some(i18n!(get_catalog(), "help_size"))),
+        Command::Help(HelpTopic::Px) => Ok(Some(i18n!(get_catalog(), "help_px"))),
+        Command::Help(HelpTopic::State) => Ok(Some(i18n!(get_catalog(), "help_state"))),
+        Command::PxGet(x, y) => match pixmap.get_pixel(x, y) {
+            Some(color) => Ok(Some(format!("PX {} {} {}", x, y, color.to_string()))),
+            None => Err("Coordinates are not inside this canvas".to_string()),
+        },
+        Command::PxSet(x, y, color) => match pixmap.set_pixel(x, y, color) {
+            true => Ok(None),
+            false => Err("Coordinates are not inside this canvas".to_string()),
+        },
+    }
+}
--- rust/src/net/tcp_server.rs
+++ rust/src/net/tcp_server.rs
@@ -40,29 +40,8 @@ async fn process_connection(mut connection: TcpConnection, pixmap: SharedPixmap)
             },
         };
 
-        // try parse the received frame as command
-        let command = match frame {
-            Frame::Simple(command_str) => match parser::simple::parse(&command_str) {
-                Ok((_, command)) => Ok(command),
-                Err(e) => Err("unhelpful, unexplained, generic error"), // TODO improve parser error handling
-            },
-        };
-
-        // handle the command and construct an appropriate response
-        let response = match command {
-            Err(e) => Some(Frame::Simple(format!(
-                "There was an error parsing your command: {}",
-                e
-            ))),
-            Ok(cmd) => match handle_command(cmd, &pixmap) {
-                Err(e) => Some(Frame::Simple(format!(
-                    "There was an error handling your command: {}",
-                    e
-                ))),
-                Ok(None) => None,
-                Ok(Some(response)) => Some(Frame::Simple(response)),
-            },
-        };
+        // handle the frame
+        let response = super::handle_frame(frame, &pixmap);
 
         // send the response back to the client (if there is one)
         match response {
@@ -75,31 +54,6 @@ async fn process_connection(mut connection: TcpConnection, pixmap: SharedPixmap)
                 _ => {}
             },
         }
-    }
-}
-
-fn handle_command(
-    cmd: Command,
-    pixmap: &SharedPixmap,
-) -> core::result::Result<Option<String>, String> {
-    match cmd {
-        Command::Size => Ok(Some(format!(
-            "SIZE {} {}",
-            pixmap.get_size().0,
-            pixmap.get_size().1
-        ))),
-        Command::Help(HelpTopic::General) => Ok(Some(i18n!(get_catalog(), "help_general"))),
-        Command::Help(HelpTopic::Size) => Ok(Some(i18n!(get_catalog(), "help_size"))),
-        Command::Help(HelpTopic::Px) => Ok(Some(i18n!(get_catalog(), "help_px"))),
-        Command::Help(HelpTopic::State) => Ok(Some(i18n!(get_catalog(), "help_state"))),
-        Command::PxGet(x, y) => match pixmap.get_pixel(x, y) {
-            Some(color) => Ok(Some(format!("PX {} {} {}", x, y, color.to_string()))),
-            None => Err("Coordinates are not inside this canvas".to_string()),
-        },
-        Command::PxSet(x, y, color) => match pixmap.set_pixel(x, y, color) {
-            true => Ok(None),
-            false => Err("Coordinates are not inside this canvas".to_string()),
-        },
     }
 }
 
