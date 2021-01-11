79017b4dffe01d00369195054d556fb43dbd62db
Finn-Thorben Sell <dev@finn-thorben.me>
2021-01-11 14:04:48 +0100

    finish implementation of rgb64 encoder

--- rust/src/state_encoding/rgb64.rs
+++ rust/src/state_encoding/rgb64.rs
@@ -1,6 +1,7 @@
 use super::SharedMultiEncodings;
 use crate::pixmap::SharedPixmap;
 use bytes::Bytes;
+use std::sync::atomic::Ordering;
 use tokio::prelude::*;
 use tokio::time::{interval, Duration};
 
@@ -20,7 +21,53 @@ pub async fn run_encoder(encodings: SharedMultiEncodings, pixmap: SharedPixmap) 
 }
 
 fn encode(pixmap: &SharedPixmap) -> Encoding {
-    let data = pixmap.get_raw_data();
-    let tmp_data = Vec::with_capacity()
+    let mut data = Vec::with_capacity(pixmap.get_size().0 * pixmap.get_size().1 * 3);
+
+    for i in pixmap.get_raw_data() {
+        let color = i.load(Ordering::SeqCst).to_le_bytes();
+        data.push(color[0]);
+        data.push(color[1]);
+        data.push(color[2]);
+    }
+
     base64::encode(&data)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::pixmap::Color;
+    use quickcheck::TestResult;
+
+    #[test]
+    fn test_encoded_content_has_correct_length() {
+        let pixmap = SharedPixmap::default();
+        let encoded = encode(&pixmap);
+        let encoded_bytes = base64::decode(&encoded).unwrap();
+        assert_eq!(
+            encoded_bytes.len(),
+            pixmap.get_size().0 * pixmap.get_size().1 * 3
+        )
+    }
+
+    quickcheck! {
+        fn test_encoded_color_is_correctly_decodable(x: usize, y: usize, color: u32) -> TestResult {
+            // prepare
+            let mut pixmap = SharedPixmap::default();
+            let color = color.into();
+            if !pixmap.set_pixel(x, y, color) {
+                return TestResult::discard()
+            }
+
+            // execute
+            let encoded = encode(&pixmap);
+            let encoded_bytes = base64::decode(&encoded).unwrap();
+
+            // verify
+            let i  = (y * pixmap.get_size().0 + x) * 3;
+            let encoded_color = &encoded_bytes[i..i+3];
+            let decoded_color = Color(encoded_color[0], encoded_color[1], encoded_color[2]);
+            TestResult::from_bool(decoded_color == color)
+        }
+    }
 }
